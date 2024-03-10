b9ccbdd52b5b33f1bb3d6665e6f22b760a1f410b
Kiri111enz <omelyanenkokirill@gmail.com>
2024-03-10 21:07:39 +0300

    dummy states and commands

--- src/bot.py
+++ src/bot.py
@@ -1,25 +1,76 @@
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, Router
+from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.storage.memory import MemoryStorage
 import aiogram.utils.markdown as md
-from env import config
+from aiogram.types import Message
 
-_dp = Dispatcher()
-
-
-@_dp.message(CommandStart())
-async def _start_handler(message: Message) -> None:
-    await message.answer(f'Привет, {md.bold(message.from_user.full_name)}\!', parse_mode='MarkdownV2')
+_router = Router()
 
 
-@_dp.message()
-async def _echo_handler(message: Message) -> None:
+class States(StatesGroup):
+    location = State()
+    period = State()
+
+
+@_router.message(CommandStart())
+async def _start_handler(message: Message, state: FSMContext) -> None:
+    await state.set_data({'period': 1})
+    await state.set_state(States.location)
+    await message.answer(f'Привет, {md.bold(message.from_user.full_name)}\\! Твоя локация?')
+
+
+@_router.message(Command('location'))
+async def _location_handler(message: Message, state: FSMContext) -> None:
+    await state.set_state(States.location)
+    await message.answer('Твоя локация?')
+
+
+@_router.message(States.location)
+async def _location_state_handler(message: Message, state: FSMContext) -> None:
+    await state.update_data(location=message.text)
+    await state.set_state()
+    await message.answer('Готово\\! Можешь узнавать погоду\\.')
+
+
+@_router.message(Command('period'))
+async def _period_handler(message: Message, state: FSMContext) -> None:
+    await state.set_state(States.period)
+    await message.answer('На какое количество дней ты хочешь узнавать погоду?')
+
+
+@_router.message(States.period)
+async def _period_state_handler(message: Message, state: FSMContext) -> None:
     try:
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        await message.answer('Неплохая попытка!')
+        period = int(message.text)
+        if period < 1 or period > 30:
+            await message.answer('Введи число от 1 до 30\\.')
+            return
+    except ValueError:
+        await message.answer('Попробуй ввести число заново\\.')
+        return
+
+    await state.update_data(period=period)
+    await state.set_state()
+    await message.answer('Период успешно установлен\\.')
 
 
-async def start() -> None:
-    bot = Bot(token=config.BOT_TOKEN)
-    await _dp.start_polling(bot)
+@_router.message(Command('weather'))
+async def _weather_handler(message: Message, state: FSMContext) -> None:
+    data = await state.get_data()
+    print(data)
+    await message.answer(f'Сегодня в городе {data["location"]} 0 градусов\\.')
+
+
+@_router.message(Command('clothes'))
+async def _clothes_handler(message: Message) -> None:
+    await message.answer('Сегодня лучше надеть шубу\\.')
+
+
+async def start(bot_token: str) -> None:
+    bot = Bot(token=bot_token, parse_mode='MarkdownV2')
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(_router)
+
+    await dp.start_polling(bot)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import logging
 import asyncio
 from bot import start as start_bot
+from env import config
 
 
 def main() -> None:
     while True:
         try:
-            asyncio.run(start_bot())
+            asyncio.run(start_bot(config.BOT_TOKEN))
         except Exception as e:
             logging.error(e)
 
